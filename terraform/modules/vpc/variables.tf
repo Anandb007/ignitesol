33379d06b374486090a79da3a10cdfdabb3e0815
anand <anand@gmail.com>
2026-05-15 16:06:49 +0000

    Created terraform remote backend VPC module

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment Name"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public Subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private Subnet CIDRs"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability Zones"
+  type        = list(string)
+}
